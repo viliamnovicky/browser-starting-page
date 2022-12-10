7a2b92dc4992b55c02278ff513531c9d37f684d1
viliamnovicky <viliamnovicky@gmail.com>
2022-12-10 21:51:10 +0100

    weather search bar

--- js/geo.js
+++ js/geo.js
@@ -1,3 +1,14 @@
+const cities = function() {
+    fetch(`../data/cities.json`)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+}
+
+cities()
 
 
 
