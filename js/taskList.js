ce082ad9458eb73e57d3a956a00ffd7a3ae2ccc4
viliamnovicky <viliamnovicky@gmail.com>
2022-12-16 22:48:35 +0100

    design of task list

--- js/taskList.js
+++ js/taskList.js
@@ -1,10 +1,28 @@
-let taskList = [];
+const addTaskBTN = document.querySelector(".btn__add-task")
+const deleteTaskBTN = document.querySelector(".btn__delete-task")
+const createTaskBTN = document.querySelector(".btn__new-task")
 
-const createTask = function() {
+const newTaskCont = document.querySelector(".task-list__new-task")
+
+addTaskBTN.addEventListener("click", function() {
+    newTaskCont.classList.remove("hidden")
+    newTaskCont.classList.remove("hidden__no-space")
+    addTaskBTN.classList.add("hidden")
+})
+
+createTaskBTN.addEventListener("click", function() {
+    newTaskCont.classList.add("hidden")
+    newTaskCont.classList.add("hidden__no-space")
+    addTaskBTN.classList.remove("hidden")
+})
+
+// let taskList = [];
+
+// const createTask = function() {
     
-}
-localStorage.setItem("names", JSON.stringify(names));
+// }
+// localStorage.setItem("names", JSON.stringify(names));
 
-//...
-var storedNames = JSON.parse(localStorage.getItem("names"));
-console.log(storedNames);
+// //...
+// var storedNames = JSON.parse(localStorage.getItem("names"));
+// console.log(storedNames);
